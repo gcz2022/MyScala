2246d61c056a25a842cb1e2d5a39e86fa61ea8c7
GuoChenzhao <chenzhao.guo@intel.com>
2018-01-22 23:00:13 +0800

    Refactor

--- src/ConcurrencyTest.scala
+++ src/ConcurrencyTest.scala
@@ -11,6 +11,13 @@ object ConcurrencyTest {
   val formats = List("oap")
   val tables = List("store_sales")
   val indices = List("store_sales_ss_item_sk1_index", "store_sales_ss_ticket_number_index")
+  val futures = mutable.ArrayBuffer[(Future[Int], String)]()
+
+  def addToFutures[T](func: => Int, hints: String) = {
+    futures += Future {
+      func
+    } -> hints
+  }
 
   def addPrefix(cmd: String, database: String): Seq[String] = {
     Seq("beeline", "-u", s"jdbc:hive2://localhost:10000/$database", "-e", cmd)
@@ -41,7 +48,7 @@ object ConcurrencyTest {
     res
   }
 
-  def waitForTheEndAndPrintResAndClear(futures: mutable.ArrayBuffer[(Future[Int], String)]) = {
+  def waitForTheEndAndPrintResAndClear = {
     futures.foreach {
       future =>
         Await.result(future._1, Duration.Inf)
@@ -54,25 +61,19 @@ object ConcurrencyTest {
   }
 
   def dropDataAndRefreshIndex(index: String, table: String, database: String) = {
-    val futures = mutable.ArrayBuffer[(Future[Int], String)]()
-    futures += Future {
-      dropTableCmd(table, database) !
-    } -> s"drop table $table, database $database"
-    futures += Future {
-      refreshIndexCmd(index, table, database) !
-    } -> s"Refresh index $index of table $table, database $database"
-    waitForTheEndAndPrintResAndClear(futures)
+    addToFutures(dropTableCmd(table, database) !,
+      s"drop table $table, database $database")
+    addToFutures(refreshIndexCmd(index, table, database) !,
+      s"Refresh index $index of table $table, database $database")
+    waitForTheEndAndPrintResAndClear
   }
 
   def dropIndicesFromSameTable(indices: Seq[String], table: String, database: String) = {
-    val futures = mutable.ArrayBuffer[(Future[Int], String)]()
-    futures += Future {
-      dropIndexCmd(indices(0), table, database) !
-    } -> s"Drop index ${indices(0)} of table $table, database $database"
-    futures += Future {
-      dropIndexCmd(indices(1), table, database) !
-    } -> s"Drop index ${indices(1)} of table $table, database $database"
-    waitForTheEndAndPrintResAndClear(futures)
+    addToFutures(dropIndexCmd(indices(0), table, database) !,
+      s"Drop index ${indices(0)} of table $table, database $database")
+    addToFutures(dropIndexCmd(indices(1), table, database) !,
+      s"Drop index ${indices(1)} of table $table, database $database")
+    waitForTheEndAndPrintResAndClear
   }
 
   def main(args: Array[String]) = {
