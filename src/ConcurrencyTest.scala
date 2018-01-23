189bcc61f1ccfa5c600e28351ebd0605b2b1077a
GuoChenzhao <chenzhao.guo@intel.com>
2018-01-23 14:15:53 +0800

    Full cases

--- src/ConcurrencyTest.scala
+++ src/ConcurrencyTest.scala
@@ -20,6 +20,14 @@ object ConcurrencyTest {
   val INSERT_DATA = 2
   val DROP_DATA = 3
 
+  val indexHintsMap = mutable.HashMap[Int, String]()
+  indexHintsMap += REFRESH_INDEX -> "refresh index"
+  indexHintsMap += DROP_INDEX -> "drop index"
+  val dataHintsMap = mutable.HashMap[Int, String]()
+  dataHintsMap += SCAN_DATA -> "scan data"
+  dataHintsMap += INSERT_DATA -> "insert data"
+  dataHintsMap += DROP_DATA -> "drop data"
+
   def addToFutures[T](func: => Int, hints: String) = {
     futures += Future {
       func
@@ -30,20 +38,64 @@ object ConcurrencyTest {
     Seq("beeline", "-u", s"jdbc:hive2://localhost:10000/$database", "-e", cmd)
   }
 
-  def dropTableCmd(table: String, database: String) = {
-    val cmd = s"drop table $table"
-    val res = addPrefix(cmd, database)
-    println(s"running: $res")
-    res
-  }
+  def testDataAndIndexOperation(
+    codeForData: Int,
+    codeForIndex: Int,
+    index: String,
+    table: String,
+    database: String,
+    dataOperationHint: String,
+    indexOperationHint: String) = {
 
-  def refreshIndexCmd(index: String, table: String, database: String) = {
-    val cmd = s"""
-                 |refresh oindex ${index} on ${table}
-                 |""".stripMargin
-    val res = addPrefix(cmd, database)
-    println(s"running: $res")
-    res
+    def scanDataCmd(index: String, table: String, database: String) = {
+      val cmd = s"select * from table $table where $index < 200"
+      val res = addPrefix(cmd, database)
+      println(s"running: $res")
+      res
+    }
+
+    def insertDataCmd(table: String, database: String) = {
+      def times24(x: Int) = Seq.fill(24)(x).mkString("(", ",", ")")
+      val insertedData = (1 to 100).foldLeft("")((str: String, x: Int) => s"$str, ${times24(x)}").tail
+      val cmd = s"insert into table $table values $insertedData"
+      val res = addPrefix(cmd, database)
+      println(s"running: $res")
+      res
+    }
+
+    def dropDataCmd(table: String, database: String) = {
+      val cmd = s"drop table $table"
+      val res = addPrefix(cmd, database)
+      println(s"running: $res")
+      res
+    }
+
+    def refreshIndexCmd(index: String, table: String, database: String) = {
+      val cmd = s"""
+                   |refresh oindex ${index} on ${table}
+                   |""".stripMargin
+      val res = addPrefix(cmd, database)
+      println(s"running: $res")
+      res
+    }
+
+    codeForData match {
+      case SCAN_DATA =>
+        addToFutures(scanDataCmd(index, table, database) !, dataOperationHint)
+      case INSERT_DATA =>
+        addToFutures(insertDataCmd(table, database) !, dataOperationHint)
+      case DROP_DATA =>
+        addToFutures(dropDataCmd(table, database) !, dataOperationHint)
+    }
+
+    codeForIndex match {
+      case REFRESH_INDEX =>
+        addToFutures(refreshIndexCmd(index, table, database) !, indexOperationHint)
+      case DROP_INDEX =>
+        addToFutures(dropIndexCmd(index, table, database) !, indexOperationHint)
+    }
+
+    waitForTheEndAndPrintResAndClear
   }
 
   def dropIndexCmd(index: String, table: String, database: String) = {
@@ -68,11 +120,11 @@ object ConcurrencyTest {
   }
 
   def dropDataAndRefreshIndex(index: String, table: String, database: String) = {
-    addToFutures(dropTableCmd(table, database) !,
-      s"drop table $table, database $database")
-    addToFutures(refreshIndexCmd(index, table, database) !,
-      s"Refresh index $index of table $table, database $database")
-    waitForTheEndAndPrintResAndClear
+    testDataAndIndexOperation(
+      DROP_DATA, REFRESH_INDEX,
+      index, table, database,
+      s"drop table $table, database $database",
+      s"Refresh index $index, table $table, database $database")
   }
 
   def dropIndicesFromSameTable(indices: Seq[String], table: String, database: String) = {
@@ -88,8 +140,16 @@ object ConcurrencyTest {
       val database = s"${format}tpcds${scale}"
       for (table <- tables) {
         val index = indices(0)
-        dropIndicesFromSameTable(indices, table, database)
-        dropDataAndRefreshIndex(index, table, database)
+//        dropIndicesFromSameTable(indices, table, database)
+//        dropDataAndRefreshIndex(index, table, database)
+        for (indexOps <- Seq(REFRESH_INDEX, DROP_INDEX)) {
+          for (dataOps <- Seq(SCAN_DATA, INSERT_DATA, DROP_DATA)) {
+            val indexHint = indexHintsMap(indexOps)
+            val dataHint = dataHintsMap(dataOps)
+            println(s"********** Testing $indexHint & $dataHint **********")
+            testDataAndIndexOperation(dataOps, indexOps, index, table, database, dataHint, indexHint)
+          }
+        }
       }
     }
   }
